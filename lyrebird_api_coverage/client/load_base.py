afa55a455e206840a59f9ef089f04ae75f68805c
Qi Gao <hljdqgq@163.com>
2018-10-29 11:33:04 +0800

    fix file io bug (#8)
    * fix file io bug
    
    * fix load file io bug

--- lyrebird_api_coverage/client/load_base.py
+++ lyrebird_api_coverage/client/load_base.py
@@ -34,10 +34,8 @@ def auto_load_base():
     # 通过本地默认base文件获取base
     elif not os.path.exists(DEFAULT_BASE):
         copy_file(DEFAULT_BASE)
-    if not codecs.open(DEFAULT_BASE, 'r', 'utf-8').read():
-        get_logger().error('Base is None.Please check your base file of API-Coverage.')
-    else:
-        json_obj = json.loads(codecs.open(DEFAULT_BASE, 'r', 'utf-8').read())
+    with codecs.open(DEFAULT_BASE, 'r', 'utf-8') as f:
+        json_obj = json.load(f)
         app_context.base_sha1 = get_file_sha1(DEFAULT_BASE)
         return json_obj
 
