1a4c050de8f8ac1fba537fb50671fd9fa7490b6c
yangmenger <yangmeng0926@163.com>
2018-11-05 19:19:02 +0800

    verify_parameter_name

--- lyrebird_api_coverage/client/url_compare.py
+++ lyrebird_api_coverage/client/url_compare.py
@@ -4,6 +4,19 @@ from urllib import parse
 # TODO 校验URL是否相等 需要把{num}情况一起放在这里解决
 
 def compare_query(url_org, url_compare):
+    param_org = parse.parse_qs(parse.urlparse(url_org).query, True)
+    param_compare = parse.parse_qs(parse.urlparse(url_compare).query, True)
+    key_compare = []
+    key_org = []
+    #处理base中存在只有参数名的情况
+    for key,value in param_org.items():
+        if not value[0].strip():
+            key_org.append(key)
+    if len(key_org):
+        for key in param_compare.keys():
+            key_compare.append(key)
+        if not set(key_org).issubset(set(key_compare)):
+            return False
     parts_org = set(parse.parse_qsl(parse.urlparse(url_org).query))
     parts_compare = set(parse.parse_qsl(parse.urlparse(url_compare).query))
     if parts_org.issubset(parts_compare):
--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,2 +1,30 @@
-def test_something():
-    assert True
+from lyrebird_api_coverage.client.url_compare import compare_query
+
+
+base_url1 = "apitrip.meituan.com/freetour?a=b&b=&c="
+base_url2 = "apitrip.meituan.com/freetour?a=&c=&b="
+base_url3 = "apitrip.meituan.com/freetour?a=b&b=c&c="
+base_url4 = "apitrip.meituan.com/freetour?a=b&b=c&c=e"
+base_url5 = "apitrip.meituan.com/freetour?a=b&b="
+req_url_1 = "http://apitrip.meituan.com/freetour?a=b&c=e&b=d"
+req_url_2 = "http://apitrip.meituan.com/freetour?a=c&b=d&c=e"
+req_url_3 = "http://apitrip.meituan.com/freetour?a=b&b=c"
+req_url_4 = "http://apitrip.meituan.com/freetour?a=b&b=c&c="
+req_url_5 = "http://apitrip.meituan.com/freetour?a=b&b=&c=e&f=q"
+
+
+def test_compare():
+    result1 = compare_query(base_url1, req_url_1)
+    assert result1
+
+    result2 = compare_query(base_url2, req_url_2)
+    assert result2
+
+    result3 = compare_query(base_url3, req_url_3)
+    assert not result3
+
+    result4 = compare_query(base_url4, req_url_4)
+    assert not result4
+
+    result5 = compare_query(base_url5, req_url_5)
+    assert result5
