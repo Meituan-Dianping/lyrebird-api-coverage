2b6391b941868e11b820e581190f78c07b9c4ea6
youngfreeFJS <471011042@qq.com>
2020-03-06 16:19:06 +0800

    apicoverage restructure v2.0 - update eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-  ],
+  extends: ["plugin:vue/essential", "airbnb-base"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ["vue"],
   rules: {
     quotes: [1, "single"], //引号类型
     "quote-props": [2, "as-needed"], // 双引号自动变单引号
@@ -25,5 +20,13 @@ module.exports = {
     "comma-dangle": [1, "always-multiline"], // 对象或数组多行写法时，最后一个值加逗号
     "space-after-keywords": [0, "always"], //关键字后面是否要空一格
     "comma-dangle": [2, "always-multiline"], // 逗号
-  },
+    "no-param-reassign": 0, //禁止给参数重新赋值
+    "no-bitwise": 0, //禁止使用按位运算符
+    "no-restricted-syntax": 0, 
+    "vue/no-side-effects-in-computed-properties": 0,
+    "import/no-unresolved": 0,
+    "no-prototype-builtins": 0,
+    "consistent-return":0
+    
+  }
 };
