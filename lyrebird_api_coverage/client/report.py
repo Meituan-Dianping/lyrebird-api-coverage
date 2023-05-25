e6a1b7b870c1db95d85709f84e965393c26998dd
lingdu <15510310709@163.com>
2023-05-25 21:11:54 +0800

    fix(rxl): report file modifyy method

--- lyrebird_api_coverage/client/report.py
+++ lyrebird_api_coverage/client/report.py
@@ -1,5 +1,5 @@
 from lyrebird_api_coverage.client.context import app_context
-from lyrebird import report
+from lyrebird import publish
 
 """
 上报处理器，用户请求行为上报到ELK中
@@ -7,7 +7,7 @@ ps:需要在lyrebird中设定reporter相关配置
 """
 
 class ReportHandler:
-    def check_url_info(self, url, device_ip):
+    def check_url_info(self, url, device_ip, category):
         specific_list = list(filter(lambda x: x.get('url') == url, app_context.merge_list))
         if specific_list and specific_list[0].get('status') == 1:
             desc = specific_list[0].get('desc')
@@ -18,20 +18,19 @@ class ReportHandler:
             count_flag = -1
             priority = -1
         info_dict = {
-            'event':{
-                'coverage':{
-                    'url': url,
-                    'desc': desc,
-                    'priority': priority,
-                    'count_flag': count_flag,
-                    'version_name': app_context.version_name,
-                    'version_code': app_context.version_code
-                }
+            'coverage':{
+                'url': url,
+                'desc': desc,
+                'priority': priority,
+                'count_flag': count_flag,
+                'version_name': app_context.version_name,
+                'version_code': app_context.version_code,
+                'category': category
             }
         }
         if app_context.info.get(device_ip):
             # 如果有Device信息，就上报device相关的信息
-            info_dict['event']['coverage'].update(app_context.info.get(device_ip))
+            info_dict['coverage'].update(app_context.info.get(device_ip))
         return info_dict
 
 
@@ -39,6 +38,6 @@ report_handler = ReportHandler()
 
 
 def report_worker(url, device_ip, category):
-    update_data = report_handler.check_url_info(url, device_ip)
-    update_data.update({'channel': 'coverage', 'category': category})
-    report(update_data)
+    update_data = report_handler.check_url_info(url, device_ip, category)
+    update_data.update({'category': category})
+    publish('coverage', update_data)
